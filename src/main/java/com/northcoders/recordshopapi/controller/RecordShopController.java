7f8abac34a6342e97ec49bc06e25f035e9b4e0eb
israelopeters <israelopeters@gmail.com>
2024-06-08 21:47:21 +0100

    feat: create endpoint to GET all albums in control layer

--- src/main/java/com/northcoders/recordshopapi/controller/RecordShopController.java
+++ src/main/java/com/northcoders/recordshopapi/controller/RecordShopController.java
@@ -1,9 +1,27 @@
 package com.northcoders.recordshopapi.controller;
 
+import com.northcoders.recordshopapi.model.Album;
+import com.northcoders.recordshopapi.service.RecordShopService;
+import com.northcoders.recordshopapi.service.RecordShopServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1/albums")
 public class RecordShopController {
+
+    @Autowired
+    RecordShopService recordShopService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Album>> getAllAlbums() {
+        return new ResponseEntity<>(recordShopService.getAllAlbums(), HttpStatus.OK);
+    }
 }
---
+++ src/test/java/com/northcoders/recordshopapi/controller/RecordShopControllerTest.java
@@ -0,0 +1,71 @@
+package com.northcoders.recordshopapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.recordshopapi.model.Album;
+import com.northcoders.recordshopapi.repository.RecordShopRepository;
+import com.northcoders.recordshopapi.service.RecordShopService;
+import com.northcoders.recordshopapi.service.RecordShopServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
+import static com.northcoders.recordshopapi.model.Album.Genre.JAZZ;
+import static com.northcoders.recordshopapi.model.Album.Genre.ROCK;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class RecordShopControllerTest {
+
+    @Mock
+    private RecordShopServiceImpl recordShopServiceImpl;
+
+    @InjectMocks
+    private RecordShopController recordShopController;
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    public void setup(){
+        mockMvcController = MockMvcBuilders.standaloneSetup(recordShopController).build();
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    @DisplayName("GET / returns a list of all albums")
+    void getAllAlbums() throws Exception {
+        //Arrange
+        List<Album> albums = List.of(
+                new Album(1L, "Album1", "AlbumName1", ROCK, LocalDate.of(2001, 1, 1), 10, "Good Album1", 5),
+                new Album(2L, "Album2", "AlbumName2", JAZZ, LocalDate.of(2002, 2, 2), 20, "Fine Album2", 7),
+                new Album(3L, "Album3", "AlbumName3", JAZZ, LocalDate.of(2003, 3, 3), 30, "Great Album3", 9)
+        );
+        when(recordShopServiceImpl.getAllAlbums()).thenReturn(albums);
+
+        //Act and Assert
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/albums/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Album2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].genre").value("JAZZ"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].quantity").value(5));
+
+    }
+}
